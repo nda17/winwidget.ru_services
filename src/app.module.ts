2478ca3f1439839f958a3234cdf9bfe980520c76
Dmitry <khv1702@gmail.com>
2026-04-17 20:51:36 +0300

    Update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { AuthModule } from '@/auth/auth.module';
 import { getGoogleRecaptchaConfig } from '@/config/google-recaptcha.config';
 import { FileModule } from '@/file/file.module';
+import { LegalPagesModule } from '@/legal-pages/legal-pages.module';
+import { NotesModule } from '@/notes/notes.module';
 import { PaymentModule } from '@/payment/payment.module';
+import { SiteSettingsModule } from '@/site-settings/site-settings.module';
 import { StatisticsModule } from '@/statistics/statistics.module';
+import { SubscriptionModule } from '@/subscription/subscription.module';
 import { UserModule } from '@/user/user.module';
+import { WidgetModule } from '@/widget/widget.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
@@ -22,7 +27,12 @@ import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
 		StatisticsModule,
 		UserModule,
 		FileModule,
-		PaymentModule
+		PaymentModule,
+		SubscriptionModule,
+		WidgetModule,
+		SiteSettingsModule,
+		LegalPagesModule,
+		NotesModule
 	]
 })
 export class AppModule {}
