783a017468aa3138f20d05babccb228d7630f74f
Dmitry <khv1702@gmail.com>
2026-05-07 23:01:55 +0300

    Fix inpit bugs

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -3,15 +3,18 @@ import {
 	IsEmail,
 	IsOptional,
 	IsString,
-	Matches
+	Matches,
+	ValidateIf
 } from 'class-validator';
 
 export class UpdateUserDto {
 	@IsOptional()
+	@ValidateIf((_, value) => value !== '')
 	@IsEmail()
 	email?: string;
 
 	@IsOptional()
+	@ValidateIf((_, value) => value !== '')
 	@Matches(/^[0-9+()\-\s]{10,20}$/)
 	phone?: string;
 
