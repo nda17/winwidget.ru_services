73c55cce73b79292e0c716a4323fd3a6b6c8ed3a
Dmitry <khv1702@gmail.com>
2026-08-26 22:34:19 +0300

    fix(ci): isolate service lint configurations

--- apps/billing/.eslintrc.cjs
+++ apps/billing/.eslintrc.cjs
@@ -1,8 +1,18 @@
 module.exports = {
-	extends: ['../../.eslintrc.js'],
+	root: true,
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		project: 'tsconfig.json',
+		project: './tsconfig.json',
 		tsconfigRootDir: __dirname,
 		sourceType: 'module'
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+	env: { node: true, jest: true },
+	ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off'
 	}
 };
--- apps/identity/.eslintrc.cjs
+++ apps/identity/.eslintrc.cjs
@@ -7,16 +7,12 @@ module.exports = {
 		sourceType: 'module'
 	},
 	plugins: ['@typescript-eslint'],
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
-	],
+	extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
 	env: { node: true, jest: true },
 	ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
 	rules: {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'prettier/prettier': 'off'
+		'@typescript-eslint/no-explicit-any': 'off'
 	}
 };
